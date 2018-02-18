6dfb979670cd859ae1df62267ae98074724b2ddb
Bhavan Channira <bhavant@gmail.com>
2018-02-18 12:48:34 -0500

    Changes done to Check Script.

--- checkAJAX.js
+++ checkAJAX.js
@@ -1,10 +1,16 @@
 var checker = function(window){
     console.log(window.isAJAXDone);
-    counter = 1;
+    var counter = 1;
+//	Set the max checking iterations to be doe by the setInterval.	
+	var maxChecks = 10;
     var checker = setInterval(
         function checkerAJAX() {
             counter++;
-            if (window.isAJAXDone === 0 || counter > 10) {
+			if (counter > maxChecks) {
+				console.log("AJAX Not responding or very slow! Please check the server. Stopping AJAX monitor.")
+				clearInterval(checker);
+			}
+            if (window.isAJAXDone === 0) {
                 console.log("AJAX Checked and found to be completed!!")
                 clearInterval(checker);
             } else {
